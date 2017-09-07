7decb81cc8bef7ba38ba81b16ae42e369aa224fe
Dmytro Bairak <dmitroid@gmail.com>
2017-09-07 15:59:51 +0300

    teacher result version #1

--- 18_test_practice_12_samaya_dlinnaya_podstroka.php
+++ 18_test_practice_12_samaya_dlinnaya_podstroka.php
@@ -1,18 +1,35 @@
 <?php
 /*
-
+Реализуйте функцию longestLength принимающую на вход строку и возвращающую длину максимальной последовательности из неповторяющихся символов.
+Подстрока может состоять из одного символа. Например в строке qweqrty, можно выделить следующие подстроки: qwe, weqrty.
+Самой длинной будет weqrty.
+Пример:
+longestLength('abcdeef'); // 5
+longestLength('jabjcdel'); // 7
 */
-$test = 'abcdeef';
+$test = 'jabjcdel';
+
 function longestLength($str)
 {
-    $str = str_split($str);
+    $map = array();
+    $max = 0;
+    $length = strlen($str);
+    $j = 0;
 
-    return $str;
+    for ($i = 0; $i < $length; $i++) {
+        $char = $str[$i];
+
+        if (array_key_exists($char, $map)) {
+            $prevIdx = $map[$char];
+            $j = max($j, $prevIdx + 1);
+        }
+
+        $map[$char] = $i;
+
+        $max = max($max, $i - $j + 1);
+    }
+
+    return $max;
 }
 
-print_r();
-
-scsacacsascas
-fffffff
-
-}
+print_r(longestLength($test));
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bd
- * Date: 06.09.2017
- * Time: 16:30
- */
